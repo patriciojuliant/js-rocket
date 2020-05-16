50810920f4837f0a4f34430c2ada85298b970690
Patricio Troncoso <patriciojuliant@gmail.com>
2020-05-16 19:48:02 -0300

    Acción Boton y Separar files

---
+++ js/script.js
@@ -0,0 +1,35 @@
+console.log ('Hello There Friends!');
+
+var sayMyName = function (name) {
+    alert('Mi nonmbre es: '+ name);
+}
+
+var car = {
+    make: 'VW',
+    type: 'Polo',
+    color: 'Blue',
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats: ['seat1','seat2','seat3','seat4',],
+            
+    turnOn: function () {
+        this.isTurnedOn = true;
+    },
+
+    fly: function () {
+        alert('fly');
+    },
+
+    jump: function (){
+
+    },
+
+    switchCar: function (isON) {
+        console.log ('Switch Car is ' +isON);
+        if (isON == true) {
+            this.isTurnedOn = true;
+            } else {
+                this.isTurnedOn = false;
+            }
+        }
+        };
